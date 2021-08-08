ed86a2b97fff78a9772027df01c5c8622bdfa8a5
Tahmid Rahman <tahmid11q@gmail.com>
2021-08-08 21:12:32 +0600

    sending POST req. to backend

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RoomListView, RoomCreateView
 
 urlpatterns = [
-    path('room-list', RoomListView.as_view()),
-    path('room-create', RoomCreateView.as_view()),
+    path('room', RoomListView.as_view()),
+    path('create-room', RoomCreateView.as_view())
 ]
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -15,8 +15,8 @@ export default class HomePage extends Component {
                     <Route exact path='/'>
                         <p>This is the home page</p>
                     </Route>
-                    <Route path='/join' component={RoomJoinPage} />
-                    <Route path='/create' component={CreateRoomPage} />
+                    <Route path="/join" component={RoomJoinPage} />
+                    <Route path="/create" component={CreateRoomPage} />
                 </Switch>
             </Router>
         );
